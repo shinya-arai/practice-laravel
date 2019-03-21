c7be735c4073d924fd46266ed5c777ec15edcc40
Arai Shinya <>
2019-03-21 18:00:02 +0900

    データ関係難しい

--- app/Content.php
+++ app/Content.php
@@ -5,15 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Post extends Model
+class Content extends Model
 {
-
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
 
     protected $fillable = [
-        'title',
-        'body',
+        'body'
     ];
 }
